ea178a93d53c3fee6fc2546da43939d6cba0b02a
pacroy <pacroy@gmail.com>
2017-02-12 12:09:30 +0700

    Refactor test code by creating a factory method

--- src/RomanConverterTest.java
+++ src/RomanConverterTest.java
@@ -3,16 +3,20 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class RomanConverterTest {
-
+	
+	private RomanConverter createNewRomanConverter() {
+		return new RomanConverter();
+	}
+	
 	@Test
 	public void should_get_i_when_1() {
-		RomanConverter roman = new RomanConverter();
+		RomanConverter roman = createNewRomanConverter();
 		assertEquals("I", roman.convert(1));
 	}
 	
 	@Test
 	public void should_get_ii_when_2() {
-		RomanConverter roman = new RomanConverter();
+		RomanConverter roman = createNewRomanConverter();
 		assertEquals("II", roman.convert(2));
 	}
 
