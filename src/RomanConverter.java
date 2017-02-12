6b59c89279551590bbbd7b345b22472d669f088a
pacroy <pacroy@gmail.com>
2017-02-12 13:06:44 +0700

    Continue refactoring. Remove duplicated values in the array.
    To do so, you need to put IF logic in the while loop to check for
    special numbers like 9 and 4.
    
    Green bar!

--- src/RomanConverter.java
+++ src/RomanConverter.java
@@ -3,13 +3,18 @@ public class RomanConverter {
 
 	public String convert(int input) {
 		String output = "";
-		int[] arabic = { 10, 10-1, 5, 5-1, 1 };
-		String[] roman = { "X", "I"+"X", "V", "I"+"V", "I" };
+		int[] arabic = { 10, 5, 1 };
+		String[] roman = { "X", "V", "I" };
 		
 		for (int i=0; i<arabic.length; i++) {
 			while (input >= arabic[i]) { 
-				output += roman[i];
-				input -= arabic[i];
+				if ((i > 0) && (input == arabic[i-1] - arabic[2])) {
+					output += (roman[2] + roman[i-1]);
+					input -= (arabic[i-1] - arabic[2]);
+				} else {
+					output += roman[i];
+					input -= arabic[i];
+				}
 			}
 		}
 
