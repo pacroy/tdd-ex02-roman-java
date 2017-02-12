d4e503bfaab6253c7c31cf2a235de6ea17f4d1bd
pacroy <pacroy@gmail.com>
2017-02-12 13:14:04 +0700

    Continue refactoring by extract duplicated "arabic[i-1] - arabic[2]"
    Extract duplicated code "arabic[i-1] - arabic[2]" into a local variable
    name "special_num". This makes us to create another IF.

--- src/RomanConverter.java
+++ src/RomanConverter.java
@@ -8,13 +8,16 @@ public class RomanConverter {
 		
 		for (int i=0; i<arabic.length; i++) {
 			while (input >= arabic[i]) { 
-				if ((i > 0) && (input == arabic[i-1] - arabic[2])) {
-					output += (roman[2] + roman[i-1]);
-					input -= (arabic[i-1] - arabic[2]);
-				} else {
-					output += roman[i];
-					input -= arabic[i];
+				if (i > 0) {
+					int special_num = arabic[i-1] - arabic[2];
+					if (input == special_num) {
+						output += (roman[2] + roman[i-1]);
+						input -= special_num;
+						continue;
+					} 
 				}
+				output += roman[i];
+				input -= arabic[i];
 			}
 		}
 
