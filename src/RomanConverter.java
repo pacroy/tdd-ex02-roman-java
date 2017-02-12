353a993651646b73415ee5ebaa60625581284c19
pacroy <pacroy@gmail.com>
2017-02-12 12:23:13 +0700

    Adjust our code to identify pattern. Still green!

--- src/RomanConverter.java
+++ src/RomanConverter.java
@@ -4,9 +4,9 @@ public class RomanConverter {
 	public String convert(int i) {
 		String output = "";
 		
-		if (i == 5) return "V";
-		if (i == 4) return "IV";
-		while (i > 0) {
+		if (i >= 5) return "V";
+		if (i >= 4) return "IV";
+		while (i >= 1) {
 			output += "I";
 			i -= 1;
 		}
