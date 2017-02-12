eb1184392275d6a344db606b8aeb05cf64a60e6c
pacroy <pacroy@gmail.com>
2017-02-12 12:43:13 +0700

    Fix our production code by adding the next mapping to arrays
    Green bar again now!

--- src/RomanConverter.java
+++ src/RomanConverter.java
@@ -3,8 +3,8 @@ public class RomanConverter {
 
 	public String convert(int input) {
 		String output = "";
-		int[] arabic = { 5, 4, 1 };
-		String[] roman = { "V", "IV", "I" };
+		int[] arabic = { 9, 5, 4, 1 };
+		String[] roman = { "IX", "V", "IV", "I" };
 		
 		for (int i=0; i<arabic.length; i++) {
 			while (input >= arabic[i]) { 
