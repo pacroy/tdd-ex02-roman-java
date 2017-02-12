997638f10fbaff1275413aabd33da810c77e91a9
pacroy <pacroy@gmail.com>
2017-02-12 12:49:30 +0700

    Adjust production code to see duplicated codes in mapping arrays
    Again, it is still green.

--- src/RomanConverter.java
+++ src/RomanConverter.java
@@ -3,8 +3,8 @@ public class RomanConverter {
 
 	public String convert(int input) {
 		String output = "";
-		int[] arabic = { 10, 9, 5, 4, 1 };
-		String[] roman = { "X", "IX", "V", "IV", "I" };
+		int[] arabic = { 10, 10-1, 5, 5-1, 1 };
+		String[] roman = { "X", "I"+"X", "V", "I"+"V", "I" };
 		
 		for (int i=0; i<arabic.length; i++) {
 			while (input >= arabic[i]) { 
