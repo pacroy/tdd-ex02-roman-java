e10c442bea91be30c46c6e71b7d2f26bb2996093
pacroy <pacroy@gmail.com>
2017-02-12 12:25:19 +0700

    Keep adjusting the code and it still green
    Do you start seeing the pattern?

--- src/RomanConverter.java
+++ src/RomanConverter.java
@@ -4,8 +4,14 @@ public class RomanConverter {
 	public String convert(int i) {
 		String output = "";
 		
-		if (i >= 5) return "V";
-		if (i >= 4) return "IV";
+		if (i >= 5) { 
+			output += "V";
+			i -= 5;
+		}
+		if (i >= 4) {
+			output += "IV";
+			i -= 4;
+		}
 		while (i >= 1) {
 			output += "I";
 			i -= 1;
