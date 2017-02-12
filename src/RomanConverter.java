4b58202176c3ac47650c33729fa8df9f42269d9c
pacroy <pacroy@gmail.com>
2017-02-12 12:45:05 +0700

    Update the mapping arrays to get Input=10 works Green!

--- src/RomanConverter.java
+++ src/RomanConverter.java
@@ -3,8 +3,8 @@ public class RomanConverter {
 
 	public String convert(int input) {
 		String output = "";
-		int[] arabic = { 9, 5, 4, 1 };
-		String[] roman = { "IX", "V", "IV", "I" };
+		int[] arabic = { 10, 9, 5, 4, 1 };
+		String[] roman = { "X", "IX", "V", "IV", "I" };
 		
 		for (int i=0; i<arabic.length; i++) {
 			while (input >= arabic[i]) { 
